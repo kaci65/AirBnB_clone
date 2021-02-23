ca5058325d10037721cc32fd4be66fc99000b95d
kaci65 <wanjikukarugi@gmail.com>
2021-02-23 15:01:06 +0300

    Edit file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -23,18 +23,3 @@ class TestBaseModel(unittest.TestCase):
         aux = "[{}] ({}) {}".format(self.__class__.__name__,
                                     self.id, self.__dict__)
         self.assertEqual(print(self.basemodel), print(aux))
-
-    def test_to_dict(self):
-        """
-        Test that to_dict() returns a dictionary containing all
-        keys/values of __dict__
-        """
-        b1 = BaseModel()
-        self.assertIsNotNone(b1.to_dict())
-        self.assertTrue(type(b1.created_at), str)
-        self.assertTrue(type(b1.updated_at), str)
-        self.assertEqual(type(b1.to_dict()), dict)
-        self.assertNotEqual(b1.to_dict().get('id'), None)
-        self.assertNotEqual(b1.to_dict().get('created_at'), None)
-        self.assertNotEqual(b1.to_dict().get('updated_at'), None)
-        self.assertEqual(b1.to_dict().get('__class__'), 'BaseModel')
